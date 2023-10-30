a9bfc7e8752431cea35c57d503d148868b142446
Cristina <codingcreativo@gmail.com>
2023-10-30 15:26:55 +0100

    argomenti prima lezione

--- introduzione.py
+++ introduzione.py
@@ -1,3 +1,14 @@
-#introduzione
+#Lezioni
 
-print("hello world")
+print("Benvenuti nella prima Lezione")
+# - Argomenti
+# - Installazione Python e VSCode
+# - Introduzione a Python
+# - Stampare messaggi in output
+# - Commenti
+# - Variabili
+# - Tipi di dati
+# - Type
+# - Casting
+# - Prendere i dati in input
+# - Operazioni aritmetiche
